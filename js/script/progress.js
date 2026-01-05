be3748b5a57fb2fee1843271f062c0ddecd63f09
nutrimuitomais <viniciuscostacorreia2018@gmail.com>
2026-01-05 17:00:27 -0300

    Create progress.js

---
+++ js/script/progress.js
@@ -0,0 +1,5 @@
+// Para Fase 1, apenas marcamos como completada a primeira etapa
+document.addEventListener("DOMContentLoaded", () => {
+  const steps = document.querySelectorAll(".progress-step");
+  steps[0].classList.add("completed");
+});
