b07cf0720114ad8015cd9ac9c0fe090f394cfcd0
nutrimuitomais <viniciuscostacorreia2018@gmail.com>
2026-01-06 01:42:03 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-function selecionarServico(servico) {
-  document.getElementById('servicoSelecionado').value = servico;
+let servicoSelecionado = null;
+let valorSelecionado = null;
+
+function selecionarServico(servico, valor) {
+  servicoSelecionado = servico;
+  valorSelecionado = valor;
+
+  localStorage.setItem('servico', servico);
+  localStorage.setItem('valor', valor);
 
   document.querySelectorAll('.card').forEach(card => {
     card.classList.remove('selected');
   });
 
   event.currentTarget.classList.add('selected');
+
+  document.getElementById('btnProsseguir').disabled = false;
+}
+
+function irParaFase3() {
+  window.location.href = 'fase3.html';
 }
