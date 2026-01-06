d5ce3b68fe4f2b109352a91b019d5b2fba198001
nutrimuitomais <viniciuscostacorreia2018@gmail.com>
2026-01-06 01:09:29 -0300

    Update fase3.js

--- js/fase3.js
+++ js/fase3.js
@@ -1,4 +1,35 @@
+const servico = localStorage.getItem('servico');
+const box = document.getElementById('conteudo');
+
+let valor = 0;
+
+if (servico === 'Provas') {
+  valor = 43;
+  box.innerHTML = `
+    <label>Matrícula</label>
+    <input id="matricula">
+    <label>Quantidade de matérias</label>
+    <input id="materias" type="number">
+    <p>Acima de 12 matérias: +R$7 por matéria</p>
+  `;
+}
+
+if (servico === 'Extracurriculares') {
+  valor = 4.99;
+  box.innerHTML = `
+    <p><strong>Extracurriculares – R$ 4,99</strong></p>
+    <ul>
+      <li>Sites gratuitos</li>
+      <li>Certificados válidos</li>
+      <li>Acesso por 31 dias</li>
+      <li>Garantia de devolução</li>
+    </ul>
+  `;
+}
+
+localStorage.setItem('valor', valor);
+
 function avancar() {
-  localStorage.setItem('detalhes', detalhes.value);
+  localStorage.setItem('detalhes', box.innerText);
   window.location.href = 'fase4.html';
 }
